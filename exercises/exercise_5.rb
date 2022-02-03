e0e766b2e05786ba5f9dd050941847803164d442
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-03 17:38:33 +0000

    Adds calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total annual revenue: #{Store.sum("annual_revenue")}"
+puts "Average annual revenue: #{Store.average("annual_revenue")}"
+puts "Stores with 1M revenue: #{Store.where("annual_revenue > 1000000").count}"
